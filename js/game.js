bae90349a20a16e9bf37a96e3ea3e38752ed79ed
17thFox <elisabeta.vulpe@gmail.com>
2016-11-20 23:51:43 +0200

    #1 Pull Request - Issues fixed

--- js/game.js
+++ js/game.js
@@ -16,6 +16,8 @@
 
   const width = 20 + 1; // +1 de la endl
   const height = 11;
+  var xPlayer, yPlayer;
+
 
   const tileMapping = {
     '0': 'wall',
@@ -48,6 +50,7 @@ String.prototype.getAt = function(y, x) {
 
     var turn = 0;
     var finished = 0;
+    var ghosts = [];
 
 
     initGhostsAndPlayer();
@@ -75,7 +78,7 @@ String.prototype.getAt = function(y, x) {
       // renderGameState(currentState);
       if (finished != 0){
         finished = 0;
-        currentState = (' ' + startState).slice(1);
+        currentState = startState;
         initGhostsAndPlayer();
         renderGameState(currentState);
         return;
@@ -84,8 +87,9 @@ String.prototype.getAt = function(y, x) {
       print('Turn ' + turn);
 
       if(movePlayer()){
-          moveGhost1();
-          moveGhost2();
+        for(var i = 0; i<ghosts.length; i++){
+          moveGhost(i); 
+        }
           renderGameState(currentState);
         }
 
@@ -97,16 +101,22 @@ String.prototype.getAt = function(y, x) {
     // Render the game state once when we start
     renderGameState(currentState);
 
-    function scoreAt(y, x, state) {
+    function scoreAt(y, x) {
+      dist = initMatrix(0);
+      distanceMatrix(y, x, 1);
       return 1 * current_score -
         1.5 * distance_to('2') -
         2 / distance_to('3') -
         4 * count('2')
     }
 
-    function getMax(y, x, depth, state) {
+    function getMax(y, x, depth) {
       var max = -999999999;
       if (y <= -1 || x <= -1 || y >= height || x >= width - 1) {
+        return max;
+      }
+
+      if (currentState.getAt(y, x) == '0' || currentState.getAt(y, x) == '3') {
         return max;
       }
 
@@ -117,43 +127,23 @@ String.prototype.getAt = function(y, x) {
       viz[y][x] = 1;
 
       if (depth == 1) {
-        initDist();
-        distanceMatrix(y, x, 0);
-        console.log('dist', dist)
-        console.log('scoreAt', scoreAt(y, x, state))
-        return scoreAt(y, x, state);
+        return scoreAt(y, x);
       }
 
-      var n = getMax(y - 1, x, depth + 1);
-
-      var v = getMax(y, x - 1, depth + 1);
-
-      var e = getMax(y, x + 1, depth + 1);
-
-      var s = getMax(y + 1, x, depth + 1);
+      const deltas = [{x: -1, y: 0}, {x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}];
 
       var maxX;
       var maxY;
-      if (max < n) {
-        max = n;
-        maxX = x;
-        maxY = y - 1;
+
+      for(var i = 0; i < deltas.length; i ++){
+        var currentMax = getMax(y + deltas[i]['y'], x + deltas[i]['x'], depth + 1);
+        if (max < currentMax) {
+          max = currentMax;
+          maxX = x + deltas[i]['x'];
+          maxY = y + deltas[i]['y'];
+        }
       }
-      if (max < v) {
-        max = v;
-        maxX = x - 1;
-        maxY = y;
-      }
-      if (max < e) {
-        max = e;
-        maxX = x + 1;
-        maxY = y;
-      }
-      if (max < s) {
-        max = s;
-        maxX = x;
-        maxY = y + 1;
-      }
+
       return {
         max: max,
         maxX: maxX,
@@ -164,27 +154,15 @@ String.prototype.getAt = function(y, x) {
     var viz = {};
     var dist = {};
 
-    function initViz() {
-
+    function initMatrix(fillValue){
+      var matrix = [];
       for (var i = 0; i < height; i++) {
-        viz[i] = {};
+        matrix[i] = [];
         for (var j = 0; j < width - 1; j++) {
-          viz[i][j] = 0;
+          matrix[i][j] = fillValue;
         }
       }
-
-    }
-
-
-    function initDist() {
-
-      for (var i = 0; i < height; i++) {
-        dist[i] = {};
-        for (var j = 0; j < width - 1; j++) {
-          dist[i][j] = 0;
-        }
-      }
-
+      return matrix;      
     }
 
     function distanceMatrix(y, x, depth) {
@@ -201,22 +179,26 @@ String.prototype.getAt = function(y, x) {
         if (dist[y][x] > depth) {
             dist[y][x] = depth;
         }
-        return;
+        else{
+          return;
+        }
       }
 
       dist[y][x] = depth;
 
-      distanceMatrix(y + 1, x, depth + 1);
-      distanceMatrix(y - 1, x, depth + 1);
-      distanceMatrix(y, x + 1, depth + 1);
-      distanceMatrix(y, x - 1, depth + 1);
+
+      const deltas = [{x: -1, y: 0}, {x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}];
+
+      for(var i = 0; i < deltas.length; i ++){
+        distanceMatrix(y + deltas[i]['y'], x + deltas[i]['x'], depth + 1);
+      }
     }
 
     function distance_to(tile) {
       var minState = 99999;
       for (var i = 0; i < height; i++) {
         for (var j = 0; j < width - 1; j++) {
-          if (currentState.getAt(i, j) == tile && minState >= dist[i][j]) {
+          if (currentState.getAt(i, j) == tile && minState > dist[i][j]) {
             minState = dist[i][j];
           }
         }
@@ -238,23 +220,15 @@ String.prototype.getAt = function(y, x) {
       return sum;
     }
 
-    var lastXGhost1, lastXGhost2, lastYGhost1, lastYGhost2, xGhost1, xGhost2, yGhost1, yGhost2, xPlayer, yPlayer, tileLastGhost1, tileLastGhost2;
-
     //Gaseste cele doua fantome din matrice si le salveaza pozitiile
     function initGhostsAndPlayer() {
-        xGhost1 = undefined;
+        ghosts = [];
         turn = 0;
         current_score = 0;
       for (var i = 0; i < height; i++) {
         for (var j = 0; j < width - 1; j++) {
           if (currentState.getAt(i, j) == '3') {
-            if (xGhost1 == undefined) {
-              xGhost1 = j;
-              yGhost1 = i;
-            } else {
-              xGhost2 = j;
-              yGhost2 = i;
-            }
+            ghosts.push({lastX: j, lastY: i, x: j, y: i, tile: '1'});
           }
           if (currentState.getAt(i, j) == '4') {
             xPlayer = j;
@@ -262,51 +236,33 @@ String.prototype.getAt = function(y, x) {
           }
         }
       }
-      lastXGhost1 = xGhost1;
-      lastYGhost1 = yGhost1;
-      lastXGhost2 = xGhost2;
-      lastYGhost2 = yGhost2;
-      tileLastGhost1 = '1';
-      tileLastGhost2 = '1'; 
     }
 
     function getGhostNewPosition(y, x, lastYGhost, lastXGhost) {
       var pos = [];
 
-      if (x - 1 > -1 && lastXGhost != x - 1) {
-        if (currentState.getAt(y, x - 1) != '0') {
-          pos.push({
-            x: x - 1,
-            y: y
-          }); //v
-        }
-      }
+      const deltas = [{x: -1, y: 0}, {x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}];
 
-      if (y - 1 > -1 && lastYGhost != y - 1) {
-        if (currentState.getAt(y - 1, x) != '0') {
-          pos.push({
-            x: x,
-            y: y - 1
-          }); //n
-        }
-      }
+      for(var i = 0; i < deltas.length; i++){
+        if (x + deltas[i]['x'] >= 0 && x + deltas[i]['x'] < width - 1 && 
+          y + deltas[i]['y'] >= 0 && y + deltas[i]['y'] < height) {
 
-      if (x + 1 < width - 1 && lastXGhost != x + 1) {
-        if (currentState.getAt(y, x + 1) != '0') {
-          pos.push({
-            x: x + 1,
-            y: y
-          }); //e
-        }
-      }
+          if(lastYGhost == y + deltas[i]['y'] && deltas[i]['y'] != 0){
+            continue;
+          }
 
-      if (y + 1 < height && lastYGhost != y + 1) {
-        if (currentState.getAt(y + 1, x) != '0') {
-          pos.push({
-            x: x,
-            y: y + 1
-          }); //s
-        }
+
+          if(lastXGhost == x + deltas[i]['x'] && deltas[i]['x'] != 0){
+            continue;
+          }
+
+          if (currentState.getAt(y + deltas[i]['y'], x + deltas[i]['x']) != '0') {
+            pos.push({
+              x: x + deltas[i]['x'],
+              y: y + deltas[i]['y']
+            });
+          }
+        }        
       }
 
       if (pos.length == 0) {
@@ -322,8 +278,8 @@ String.prototype.getAt = function(y, x) {
     }
 
     function movePlayer(){
-        initViz();
-        var newPos = getMax(yPlayer, xPlayer, 0, currentState);
+        viz = initMatrix(0);
+        var newPos = getMax(yPlayer, xPlayer, 0);
 
         if( currentState.getAt(newPos['maxY'], newPos['maxX']) == '3'){
             print(" - Player moved on ghost.");
@@ -339,65 +295,59 @@ String.prototype.getAt = function(y, x) {
         if(currentState.getAt(newPos['maxY'], newPos['maxX']) == '2'){
             current_score += 1;
             print(" - Player eats a piece of food. Score is: "+current_score);
+
         }
 
         xPlayer = newPos['maxX'];
         yPlayer = newPos['maxY'];
         currentState = currentState.replaceAt(yPlayer, xPlayer, '4');
+
+        if (count('2') == 0){
+          finished = 1;
+          print(" - Player wins! Score is: "+current_score);
+          return false;
+        }
+
         return true;
     }
 
 
-    function moveGhost1(){
-        var newPos = getGhostNewPosition(yGhost1, xGhost1, lastYGhost1, lastXGhost1);
-        currentState = currentState.replaceAt(yGhost1, xGhost1, tileLastGhost1);
-        tileLastGhost1 = currentState.getAt(newPos['y'], newPos['x']);
+    function moveGhost(ghostIndex){
+        var x = ghosts[ghostIndex]['x'];
+        var y = ghosts[ghostIndex]['y'];
+        var tile = ghosts[ghostIndex]['tile'];
+        var lastY = ghosts[ghostIndex]['lastY'];
+        var lastX = ghosts[ghostIndex]['lastX'];
 
-        if(tileLastGhost1 == '4'){
-            print(" - Ghost 1 eats player character");
+        var newPos = getGhostNewPosition(y, x, lastY, lastX);
+        currentState = currentState.replaceAt(y, x, tile);
+        tile = currentState.getAt(newPos['y'], newPos['x']);
+
+        if(tile == '4'){
+            print(" - Ghost "+ ghostIndex +" eats player character");
             print(" - Ghosts win - GAME OVER");
             finished = 1;
             return;
         }
 
-        print(" - Ghost 1 moved to ["+newPos['x']+", "+ newPos['y']+"]");
-        if (tileLastGhost1 == '3'){
-            tileLastGhost1 = tileLastGhost2;
+        print(" - Ghost "+ ghostIndex +" moved to ["+newPos['x']+", "+ newPos['y']+"]");
+        
+        //Daca pe tile-ul pe care vreau sa ma mut se afla o fantoma, retin ce se afla inainte de fantoma acolo
+        if (tile == '3'){
+          for(var i = 0; i<ghosts.length; i++){
+            if(ghosts[i]['x'] == newPos['x'] && ghosts[i]['y'] == newPos['y']){
+              tile = ghosts[i]['tile'];
+            }
+          }
         }
 
-        lastXGhost1 = xGhost1;
-        lastYGhost1 = yGhost1;
-        yGhost1 = newPos['y'];
-        xGhost1 = newPos['x'];
-        currentState = currentState.replaceAt(yGhost1, xGhost1, '3');
+        ghosts[ghostIndex]['lastX'] = x;
+        ghosts[ghostIndex]['lastY'] = y;
+        ghosts[ghostIndex]['y'] = newPos['y'];
+        ghosts[ghostIndex]['x'] = newPos['x'];
+        ghosts[ghostIndex]['tile'] = tile;
+        currentState = currentState.replaceAt(ghosts[ghostIndex]['y'], ghosts[ghostIndex]['x'], '3');
     }
-
-
-    function moveGhost2(){
-        var newPos = getGhostNewPosition(yGhost2, xGhost2, lastYGhost2, lastXGhost2);
-        currentState = currentState.replaceAt(yGhost2, xGhost2, tileLastGhost2);
-        tileLastGhost2 = currentState.getAt(newPos['y'], newPos['x']);
-
-
-        if(tileLastGhost2 == '4'){
-            print(" - Ghost 2 eats player character");
-            print(" - Ghosts win - GAME OVER");
-            finished = 1;
-            return;
-        }
-
-        print(" - Ghost 2 moved to ["+newPos['x']+", "+ newPos['y']+"]");
-        if (tileLastGhost2 == '3'){
-            tileLastGhost2 = tileLastGhost1;
-        }
-
-        lastXGhost2 = xGhost2;
-        lastYGhost2 = yGhost2;
-        yGhost2 = newPos['y'];
-        xGhost2 = newPos['x'];
-        currentState = currentState.replaceAt(yGhost2, xGhost2, '3');
-    }
-
 
     function print(msg){
         var prev = $("#gameLogOutput").val();
